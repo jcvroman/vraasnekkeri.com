675bdb174bee6c22b0d7920d32e5eb35309962ad
jcvroman <jcvroman@mac.com>
2022-09-07 09:30:41 -0600

    Initial Commit

---
+++ Sources/VraasnekkeriCom/main.swift
@@ -0,0 +1,25 @@
+import Foundation
+import Publish
+import Plot
+
+// This type acts as the configuration for your website.
+struct VraasnekkeriCom: Website {
+    enum SectionID: String, WebsiteSectionID {
+        // Add the sections that you want your website to contain here:
+        case posts
+    }
+
+    struct ItemMetadata: WebsiteItemMetadata {
+        // Add any site-specific metadata that you want to use here.
+    }
+
+    // Update these properties to configure your website:
+    var url = URL(string: "https://vraasnekkeri.com")!
+    var name = "Vråsnekkeri"
+    var description = "Vråsnekkeri: Methodically crafted."
+    var language: Language { .english }
+    var imagePath: Path? { nil }
+}
+
+// This will generate your website using the built-in Foundation theme:
+try VraasnekkeriCom().publish(withTheme: .foundation)
